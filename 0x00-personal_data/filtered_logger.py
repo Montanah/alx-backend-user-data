a490c053271d439f01aad198e42301f9f2aafc2d
Montanah <mititi.isaac@gmail.com>
2023-08-31 22:30:08 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,3 +16,21 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{field}=.+?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+    class RedactingFormatter(logging.Formatter):
+        """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str] = PII_FIELDS):
+        """init method"""
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format method"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
