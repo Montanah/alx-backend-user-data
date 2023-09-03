2a668b5ae35209566f73abd01773e22046cd90b6
Montanah <mititi.isaac@gmail.com>
2023-09-03 20:57:11 +0000

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,23 +9,15 @@ import os
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
-    for field in fields:
-        message = re.sub(f'{field}=.+?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
-
-    class RedactingFormatter(logging.Formatter):
-        """ Redacting Formatter class
-        """
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: List[str] = PII_FIELDS):
         """init method"""
         super().__init__(self.FORMAT)
         self.fields = fields
@@ -35,8 +27,18 @@ def filter_datum(fields: List[str], redaction: str,
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
-    def get_logger() -> logging.Logger:
-        """returns a logging.Logger object"""
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(f'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object"""
     logger = logging.getLogger('user_data')
     logger.setLevel(logging.INFO)
     logger.propagate = False
