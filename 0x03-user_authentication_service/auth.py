7a3da6280adf80d1f4dfc896b5de04ad3731fe35
Montanah <mititi.isaac@gmail.com>
2023-09-14 22:09:09 +0000

    Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,3 +16,27 @@ def _hash_password(password: str) -> str:
     """
     import bcrypt
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Method that takes mandatory email and password string
+        arguments and returns a User object
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            pass
+
+        password = _hash_password(password)
+        user = self._db.add_user(email, password)
+
+        return user
